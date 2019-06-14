9af029867db63d0cb8a4c659ad08aaa01d2cfd79
陈明 <1106934930@qq.com>
2019-06-14 10:33:41 +0800

    第一次

---
+++ src/main/java/cn/itcast/dao/impl/UserDaoImpl.java
@@ -0,0 +1,10 @@
+package cn.itcast.dao.impl;
+
+import cn.itcast.dao.UserDao;
+
+public class UserDaoImpl  implements UserDao{
+
+    public void save() {
+        System.out.println("创建对象");
+    }
+}
--- src/main/java/cn/itcast/test/Test1.java
+++ src/main/java/cn/itcast/test/Test1.java
@@ -1,12 +1,19 @@
 package cn.itcast.test;
 
+import cn.itcast.dao.UserDao;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test1 {
 
 
     @Test
     public void test(){
-        System.out.println("HelloWord");
+        ApplicationContext app = new ClassPathXmlApplicationContext("Applicationcontext.xml");
+
+       UserDao userDao = (UserDao) app.getBean("userDao");
+       userDao.save();
+
     }
 }
